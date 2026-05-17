9883062d719498739c382cdc0ff89a4052b3bc20
davron <davrontojialiev@gmail.com>
2026-05-17 20:07:01 +0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+
+const Header = () => {
+  return (
+   <header className="header">
+      <div className="container">
+        <nav className="header__nav">
+          {/*  */}
+        </nav>
+
+      </div>
+    </header>
+  );
+};
+
+export default Header
